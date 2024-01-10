d0b7604822e244e8617eebd6006bf2782eaf97c6
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-01-10 10:17:09 +0100

    add case for https://youtrack.jetbrains.com/issue/IDEA-305312 - comment

--- micronaut-bean-injections-test/src/main/java/com/mytests/micronaut/plainFieldOrConstructorInjection/FirstBeanToInject.java
+++ micronaut-bean-injections-test/src/main/java/com/mytests/micronaut/plainFieldOrConstructorInjection/FirstBeanToInject.java
@@ -2,6 +2,8 @@ package com.mytests.micronaut.plainFieldOrConstructorInjection;
 
 import jakarta.inject.Singleton;
 
+// https://youtrack.jetbrains.com/issue/IDEA-305312:
+// click on gutter and check how the navigation candidates are displayed
 @Singleton
 public class FirstBeanToInject {
     @Override
